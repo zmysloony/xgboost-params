fix time calculation, widen generation area to 50%

performance_test.py
@@ -46,7 +46,7 @@ def perf_test(data, target, t_args: {}):
     if t_args['mutation']:
         print("Starting mutation algorithm...")
         # for part in np.arange(0.2, 0.65, 0.15):
-        for part in [0.05]:
+        for part in [0.5]:
             experiment_name = 'muta_' + str(part)
 
             if t_args['timeout']:
@@ -66,7 +66,7 @@ def perf_test(data, target, t_args: {}):
         print("Starting hill climbing algorithm...")
         #for part in np.arange(0.2, 0.65, 0.15):
             #for worse in [8, 16, 24]:
-        for part in [0.05]:
+        for part in [0.5]:
             for worse in [6]:
                 experiment_name = 'hill_' + str(part) + '_' + str(worse)
 